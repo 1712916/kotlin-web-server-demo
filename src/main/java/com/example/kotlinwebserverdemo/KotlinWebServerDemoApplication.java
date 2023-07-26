17de6c71e6654dd5cfe5d40551b66344a48a5ece
Ngo Tan Vinh <kieuphong2840@gmail.com>
2023-07-26 14:18:51 +0700

    create project by web: https://start.spring.io/

---
+++ src/main/java/com/example/kotlinwebserverdemo/KotlinWebServerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.kotlinwebserverdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KotlinWebServerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KotlinWebServerDemoApplication.class, args);
+	}
+
+}
