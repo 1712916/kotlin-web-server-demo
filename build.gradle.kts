6872a42c513e143d40eaa2870e8e899ef94edea7
Ngo Tan Vinh <kieuphong2840@gmail.com>
2023-07-27 14:43:51 +0700

    create get hello world name controller

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,10 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "3.1.2"
 	id("io.spring.dependency-management") version "1.1.2"
+	kotlin("plugin.jpa") version "1.4.10"
 	kotlin("jvm") version "1.8.22"
 	kotlin("plugin.spring") version "1.8.22"
+
 }
 
 group = "com.example"
@@ -21,6 +23,15 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("com.github.salemebo:restful-api:0.2.8")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-data-rest")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("com.h2database:h2")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
